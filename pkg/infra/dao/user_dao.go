52ea0e5dbf2f10f96274d46a4de41771f83a1f30
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-09-21 14:40:43 +0900

    [03]daoにfindByTokenを実装

--- pkg/infra/dao/user_dao.go
+++ pkg/infra/dao/user_dao.go
@@ -1,16 +1,27 @@
 package dao
 
 import (
+	"context"
 	"database/sql"
 )
 
 // UserDaoという型を定義。
 type UserDao interface {
 	Create(name, token string) error
+	FindByToken(ctx context.Context, token string) (*User, error)
 }
 
 type userDao struct {
 	db *sql.DB
+}
+
+// フィールド名大文字始まりで外部からアクセス可能。daoだけではなく、handlerからもアクセス可能にする
+type User struct {
+	ID        int
+	Name      string
+	Highscore int
+	Coin      int
+	Token     string
 }
 
 func NewUserDao(db *sql.DB) UserDao {
@@ -25,4 +36,15 @@ func (userDao *userDao) Create(name, token string) error {
 	}
 
 	return nil
+}
+
+// Tokenからユーザーを検索し、返す
+func (userDao *userDao) FindByToken(ctx context.Context, token string) (*User, error) {
+	var user User
+	err := userDao.db.QueryRowContext(ctx, "SELECT id, name, highscore, coin, token FROM users WHERE token = ?", token).Scan(&user.ID, &user.Name, &user.Highscore, &user.Coin, &user.Token) //Scanで書き込み
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
